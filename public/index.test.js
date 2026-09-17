8787f2b028a45bdbc9b96f05745a32b9550b3751
Jace OReagan <jaceoreagan@gmail.com>
2026-09-17 11:46:20 -0600

    implemented tests

---
+++ public/index.test.js
@@ -0,0 +1,14 @@
+describe("Static HTML Structure", () => {
+  beforeEach(() => {
+    document.body.innerHTML = `
+            <header>
+                <h1 id="site-title">Welcome</h1>
+            </header>
+        `;
+  });
+  test("contains the correct main body heading", () => {
+    const heading = document.getElementById("site-title");
+    expect(heading).not.toBeNull();
+    expect(heading.textContent).toBe("Welcome");
+  });
+});
